0f104828a7fcb9963f90eb5774aacd8bc4084209
karthick <karthickfaf19.7@gmail.com>
2025-02-27 00:11:14 +0530

    Create datatype and its memory .c

---
+++ datatype and its memory.c
@@ -0,0 +1,28 @@
+
+
+#include<stdio.h>
+int main(){
+    
+    char name[12];
+    printf("ENTER THE STUDENT NAME  :");
+    scanf("%s",&name);
+    
+    int studage;
+    
+    printf("ENTER THE STUFDENT AGE : ");
+    scanf("%d",&studage);
+    
+    
+    int studid;
+    printf("ENTER THE STUDENT ROLL NO :");
+    scanf("%d",&studid);
+    
+    char studgrade;
+    printf("ENTER THE STUDENT GRADE : ");
+    scanf("%c ",&studgrade);
+
+  return 0;
+    
+    
+  }
+
