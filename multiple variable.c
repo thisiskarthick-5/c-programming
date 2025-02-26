c09390106476cc9a5116de6926a95cd06624d349
karthick <karthickfaf19.7@gmail.com>
2025-02-26 23:58:42 +0530

    multiple variable .c

---
+++ multiple variable.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main(){
+    //multiple variable
+    //type 1;
+    int num1,num2,num3; //predefining
+    num1 = 1;
+    num2 = 2;
+    num3 = 3;
+    
+    int num4 = 12, num5 = 34,num6 =78;//difeining in oneline
+    
+    //assinging multiple variable to single value
+    
+    int a,b,c;
+    a = b = c = 23;
+    printf("%d\n %d\n %d\n",a,b,c);
+  }
