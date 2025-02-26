1082ffee6cc9236b226f1512945c73fff0eb8bda
karthick <karthickfaf19.7@gmail.com>
2025-02-26 23:50:42 +0530

    variables.c

---
+++ structure of c.c
@@ -0,0 +1,30 @@
+//variables
+
+//int
+//float
+//char
+
+
+
+#include<stdio.h>
+int main(){
+    int num = 12;
+    printf("%d",num); // format specifier used for int is "%d" or "%i"
+    
+    //float - 4 byte stores upto 6-7 decimal number
+    float num1 = 12.5;
+    printf("\n%f",num1); // format specifier used for the float is "%f"
+    
+    double num3 = 123.46353;
+    printf("\n%lf",num3); //format specifier used for double is "lf"
+    
+    char str = 'h';
+    printf("\n%c",str); //format specifier used for char is "%c"
+    
+    char str1[] = "karthick"; //for string we use "%s"
+    printf("\n%s",str1);
+    
+    
+    
+}
+
